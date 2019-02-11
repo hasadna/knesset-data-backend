0da92ea792b98e7c74c35decd460cd4c4e659d3a
yuvalbl <yuval.bar.levi@gmail.com>
2019-02-11 19:23:00 +0200

    * config DB service and fetch data from postgres * remove fetch service (since we now work with DB and no json)

--- api/committees/committees.data.adapter.js
+++ api/committees/committees.data.adapter.js
@@ -1,19 +1,70 @@
 const getData = require('../../services/data.service').getData;
+const getDataAndCallback = (query, route, callback) => {
+	getData(query, route).then(data => callback(data));
+};
 
 module.exports = {
-	all: (callback) => {
-		getData('committees/index.json')
-			.then(data => callback(data));
+	// get committees summary for all knessets
+	// https://app.redash.io/hasadna/queries/165441/source#284091
+	all: (route, callback) => {
+		const query = `
+		select committees.knesset_num, committees.knesset_name, committees.num_committees, sessions.num_sessions from (
+			select k.knesset_num, k.knesset_name, count(1) num_committees
+			from (
+				select "KnessetNum" knesset_num, "Name" knesset_name from knesset_kns_knessetdates
+				group by "KnessetNum", "Name"
+			) k, committees_kns_committee c
+			where c."KnessetNum" = k.knesset_num
+			group by k.knesset_num, k.knesset_name
+		) committees, (
+			select k.knesset_num, k.knesset_name, count(1) num_sessions
+			from (
+				select "KnessetNum" knesset_num, "Name" knesset_name from knesset_kns_knessetdates
+				group by "KnessetNum", "Name"
+			) k, committees_kns_committeesession s
+			where s."KnessetNum" = k.knesset_num
+			group by k.knesset_num, k.knesset_name
+		) sessions
+		where committees.knesset_num = sessions.knesset_num
+		order by knesset_num desc
+		`;
+		getDataAndCallback(query, route, callback)
 	},
-	getById: (id, callback) => {
-		getData(`committees/${id}.json`)
-			.then(data => callback(data));
+	// get committees for specific knesset
+	// https://app.redash.io/hasadna/queries/165441/source#284091
+	byKnessetId: (route, callback, knessetId) => {
+		const query = `
+		select s.num_sessions, c.*
+		from committees_kns_committee c, (
+			select s."CommitteeID" committee_id, count(1) num_sessions
+			from committees_kns_committeesession s
+			where s."KnessetNum" = ${knessetId}
+			group by s."CommitteeID"
+		) s
+		where c."CommitteeID" = s.committee_id
+		order by s.num_sessions desc
+		`;
+		getDataAndCallback(query, route, callback)
 	},
-	getByMeeting: (meetingId, callback) => {
-		const meetingRoute = meetingId.replace(/-/g, '/');
-		console.log(`meetings/${meetingRoute}.json`);
-
-		getData(`meetings/${meetingRoute}.json`)
-			.then(data => callback(data));
+	// get data for specific committee
+	// MOCK DATA
+	byCommitteeId: (route, callback, knessetId, committeeId) => {
+		const query = `
+		SELECT "CommitteeSessionID", "KnessetNum", "TypeDesc", "committee_name"
+		FROM committees_kns_committeesession
+		LIMIT 1;
+		`;
+		getDataAndCallback(query, route, callback)
+	},
+	// get data for specific committee meeting
+	// MOCK DATA
+	byMeetingId: (route, callback,  knessetId, committeeId, meetingId) => {
+		const query = `
+		SELECT "CommitteeSessionID", "KnessetNum", "TypeDesc", "committee_name"
+		FROM committees_kns_committeesession
+		WHERE "KnessetNum" = ${knessetId}
+		ORDER BY "StartDate" desc
+		`;
+		getDataAndCallback(query, route, callback)
 	}
 };
--- api/committees/committees.route.js
+++ api/committees/committees.route.js
@@ -1,27 +1,41 @@
 const router = require('express').Router();
 const collection = require('./committees.data.adapter');
 
-router.route('/committees')
-	.get((req, res) => {
-		collection.all((data) => {
-			res.json(data);
-		});
-	});
+// statistics of all committees per knesset
+// collection:{knesset-id, knesset-name, #committees-per-knesset, #meetings-per-knesset}
+router.route('/committees-by-knesset')
+    .get((req, res) => {
+        collection.all(req.route.path, (d) => res.json(d));
+    });
 
-router.route('/committees/:itemId')
-	.get((req, res) => {
-		const id = req.params['itemId'];
-		collection.getById(id, (data) => {
-			res.json(data);
-		});
-	});
+// committees data for specific knesset (id, name, meetings num)
+// single:{knesset-id, knesset-name}
+// collection:{committee-id,  committee-name, #meetings-per-knesset}
+router.route('/committees-by-knesset/:knessetId')
+    .get((req, res) => {
+        const knessetId = req.params['knessetId'];
+        collection.byKnessetId(req.route.path, (d) => res.json(d), knessetId);
+    });
 
-router.route('/committees/:itemId/:meetingId')
-	.get((req, res) => {
-		const id = req.params['meetingId'];
-		collection.getByMeeting(id, (data) => {
-			res.json(data);
-		});
-	});
+// all meetings of a specific committee (for specific knesset)
+// single:{knesset-id, knesset-name, committee-id,  committee-name, mks-id-collection}
+// collection:{meeting-id, meeting-name, meeting-date, has-protocol}
+router.route('/committees-by-knesset/:knessetId/:committeeId')
+    .get((req, res) => {
+        const knessetId = req.params['knessetId'];
+        const committeeId = req.params['committeeId'];
+        collection.byCommitteeId(req.route.path, (d) => res.json(d), knessetId, committeeId);
+    });
+
+// specific meeting data (protocol)
+// single:{knesset-id, knesset-name, committee-id,  committee-name, meeting-id, meeting-name, meeting-date, mks-id-collection, general-protocol-data}
+// collection:{speaker, mk-id(-1 if not mk), content }
+router.route('/committees-by-knesset/:knessetId/:committeeId/:meetingId')
+    .get((req, res) => {
+        const knessetId = req.params['knessetId'];
+        const committeeId = req.params['committeeId'];
+        const meetingId = req.params['meetingId'];
+        collection.byMeetingId(req.route.path, (d) => res.json(d), knessetId, committeeId, meetingId);
+    });
 
 module.exports = router;
