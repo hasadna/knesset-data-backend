7006a3afd5116a9591ab1015c0dcc2ee0dfd0d40
yuvalbl <yuval.bar.levi@gmail.com>
2019-02-25 21:36:59 +0200

    * heroku support

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use('/api', require('./api/news/news.route')); // TODO: implement route for 
 // 		res.send('ERROR');
 // 	}
 // });
-
-app.listen(4000,
-	() => debug('data server listening on port 4000. try: http://localhost:4000/api/committees-by-knesset/')
+const PORT = process.env.PORT || 3000;	//process.env.PORT is used by heroku
+app.listen(PORT,
+	() => debug(`data server listening on port ${PORT}. try: [URL:PORT]/api/committees-by-knesset/`)
 );
