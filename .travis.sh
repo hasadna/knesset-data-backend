aae955f93a16bfd0a2e5847f7dedb6123b85151b
Ori Hoch <ori@uumpa.com>
2019-03-29 20:43:29 +0300

    travis

--- .travis.sh
+++ .travis.sh
@@ -4,11 +4,11 @@ if [ "${1}" == "deploy" ]; then
     docker push "uumpa/knesset-data-backend:latest" &&\
     docker push "uumpa/knesset-data-backend:${TRAVIS_COMMIT}" &&\
     travis_ci_operator.sh github-yaml-update \
-        hasadna-k8s master values.auto-updated.yaml '{"oknesset":{"DataBackendImage": "'${DOCKER_IMAGE}:${TRAVIS_COMMIT}'"}}' \
+        hasadna-k8s master values.auto-updated.yaml '{"oknesset":{"DataBackendImage": "uumpa/knesset-data-backend:'${TRAVIS_COMMIT}'"}}' \
         "automatic update of knesset-data-backend" hasadna/hasadna-k8s &&\
     echo &&\
     echo Great Success &&\
     echo &&\
-    echo ${DOCKER_IMAGE}:latest &&\
-    echo ${DOCKER_IMAGE}:${TRAVIS_COMMIT}
+    echo uumpa/knesset-data-backend:latest &&\
+    echo uumpa/knesset-data-backend:${TRAVIS_COMMIT}
 fi
